411b351535c134e2a01ccfefa1ab874c0c3644df
Michael Ilao <34318151+michaelilao@users.noreply.github.com>
2023-07-21 00:46:15 -0400

    Autoupdate Google Drive

---
+++ src/lib/convertDocx.ts
@@ -0,0 +1,58 @@
+import mammoth from "mammoth";
+
+export const htmlToExperiences = async (buffer: Buffer) => {
+	const { value } = await mammoth.convertToHtml({ buffer: buffer });
+	let experienceRaw = value.split("<p><strong>WORK EXPERIENCE</strong></p>")[1];
+	experienceRaw = experienceRaw.split("<p><strong>PROJECTS</strong></p>")[0];
+
+	const experience = experienceRaw.split("<p><strong>");
+	const experienceList: Experience[] = experience.map((entry) => {
+		const company = entry.split("</strong></p>")[0].split("\t")[0].split("</")[0];
+
+		// Length of <p><em> is 7
+		const secondHeadingRaw = entry.substring(entry.indexOf("<p><em>") + 7, entry.lastIndexOf("</em></p>"));
+
+		const title = secondHeadingRaw.substring(0, secondHeadingRaw.indexOf("20"));
+		const dateRaw = secondHeadingRaw.substring(secondHeadingRaw.indexOf("20"));
+		const dates = dateRaw.split(/[-\u2013]/);
+		let startDate;
+		let endDate;
+		if (dates.length !== 2) {
+			startDate = dates[0];
+			endDate = dates[0];
+		} else {
+			startDate = dates[0];
+			endDate = dates[1];
+		}
+
+		const itemsRaw = entry.split("<li>");
+		const items: string[] = [];
+		const skills: string[] = [];
+
+		var regex = /<strong>(.+?)<\/strong>/g;
+
+		itemsRaw.forEach((value, index) => {
+			if (index !== 0) {
+				value.match(regex)?.forEach((skillRaw) => {
+					const skill = skillRaw.replace(/(<([^>]+)>)/gi, "").trim();
+					skills.push(skill);
+				});
+				items.push(value.replace(/(<([^>]+)>)/gi, ""));
+			}
+		});
+		const experienceEntry: Experience = {
+			id: "",
+			title: title.trim(),
+			company: company.trim(),
+			startDate: startDate.trim(),
+			endDate: endDate.trim(),
+			skills: skills,
+			items: items,
+		};
+		experienceEntry.id = experienceEntry.title + " " + experienceEntry.company;
+
+		return experienceEntry;
+	});
+
+	return experienceList.filter((experience) => experience.title);
+};
---
+++ src/lib/googleDrive.ts
@@ -0,0 +1,46 @@
+import { google } from "googleapis";
+
+export const getGoogleDriveData = async () => {
+	const credentials = {
+		type: process.env.TYPE,
+		project_id: process.env.PROJECT_ID,
+		private_key_id: process.env.PRIVATE_KEY_ID,
+		private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+		client_email: process.env.CLIENT_EMAIL,
+		client_id: process.env.CLIENT_ID,
+		auth_uri: process.env.AUTH_URI,
+		token_uri: process.env.TOKEN_URI,
+		auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+		client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+		universe_domain: process.env.UNIVERSE_DOMAIN,
+	};
+
+	const auth = new google.auth.GoogleAuth({
+		credentials: credentials,
+		scopes: ["https://www.googleapis.com/auth/drive"],
+	});
+	// allows you to use drive API methods e.g. listing files, creating files.
+	const drive = google.drive({ version: "v3", auth });
+	try {
+		const res = await drive.files.list();
+		const files = res.data.files;
+		const resume = files?.find((file) => file?.name?.includes("Michael_Ilao_Resume"));
+		if (!(resume && resume.id)) {
+			return null;
+		}
+		const getResumeData = await drive.files.get(
+			{
+				fileId: resume.id,
+				alt: "media",
+			},
+			{
+				responseType: "arraybuffer",
+			}
+		);
+		const buffer: any = getResumeData.data;
+		return buffer;
+	} catch (error: any) {
+		console.log(error);
+		return null;
+	}
+};
